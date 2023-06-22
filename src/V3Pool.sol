9427474e241c9cc231d0db190a09d1224dff69af
Anton MetaZK <shonash88@gmail.com>
2023-06-22 05:53:12 +0300

    add export and add variable

--- src/V3Pool.sol
+++ src/V3Pool.sol
@@ -1,4 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-contract SberDexV3Pool {}
+import "./lib/Tick.sol";
+import "./lib/Position.sol";
+
+contract SberDexV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    //Pool tokens, immutable
+    address public immutable token0;
+    address public immutable token1;
+
+    //Packing variable that are read together
+    struct Slot0 {
+        //Current sqrt (P)
+        uint160 sqrtPriceX96;
+        //Current Tick
+        uint24 tick;
+    }
+
+    Slot0 public slot0;
+
+    //Amount of liqudity, L
+    uint128 public liqudity;
+
+    //Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    //Position info
+    mapping(bytes32 => Position.Info) public position;
+}
