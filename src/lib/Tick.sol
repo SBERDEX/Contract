4c2a3817062d8faf33c03d3afd73147aec2b8c7f
Anton MetaZK <shonash88@gmail.com>
2023-06-23 05:49:18 +0300

    add function update on Tick.sol

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -7,4 +7,20 @@ library Tick {
         bool initialized;
         uint128 liqidity;
     }
+
+    function update(
+        mapping(int24 => Tick.Info) storage self,
+        int24 tick,
+        uint128 liqidityDelta
+    ) internal {
+        Tick.Info storage tickInfo = self[tick];
+        uint128 liquidityBefore = tickInfo.liqudity;
+        uint128 liquidityAfter = liquidityBefore + liqidityDelta;
+
+        if (liquidityBefore == 0) {
+            tickInfo.initialized = true;
+        }
+
+        tickInfo.liqidity = liquidityAfter;
+    }
 }
