a8a0d702be9760ee0374689bc92173da620cc8f7
Anton MetaZK <shonash88@gmail.com>
2023-06-26 03:59:10 +0300

    fix incorrect (liquidity) name

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -5,22 +5,22 @@ pragma solidity ^0.8.14;
 library Tick {
     struct Info {
         bool initialized;
-        uint128 liqidity;
+        uint128 liqudity;
     }
 
     function update(
         mapping(int24 => Tick.Info) storage self,
         int24 tick,
-        uint128 liqidityDelta
+        uint128 liqudityDelta
     ) internal {
         Tick.Info storage tickInfo = self[tick];
         uint128 liquidityBefore = tickInfo.liqudity;
-        uint128 liquidityAfter = liquidityBefore + liqidityDelta;
+        uint128 liquidityAfter = liquidityBefore + liqudityDelta;
 
         if (liquidityBefore == 0) {
             tickInfo.initialized = true;
         }
 
-        tickInfo.liqidity = liquidityAfter;
+        tickInfo.liqudity = liquidityAfter;
     }
 }
