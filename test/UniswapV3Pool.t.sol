1b2c092a20849370266f24edf7cb2a93c86d9c35
Anton <>
2023-06-27 03:49:22 +0300

    stable version

---
+++ test/UniswapV3Pool.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "../lib/forge-std/src/Test.sol";
+
+contract UniswapDexV3PoolTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
