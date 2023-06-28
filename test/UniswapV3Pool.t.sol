18f45e7d210f4adbf89bf165ca4673e602552bbc
Anton MetaZK <shonash88@gmail.com>
2023-06-28 03:02:10 +0300

    add test ERC20 Mintable

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -2,11 +2,29 @@
 pragma solidity ^0.8.14;
 
 import "../lib/forge-std/src/Test.sol";
+import "./ERC20Mintable.sol";
 
 contract UniswapDexV3PoolTest is Test {
-    function setUp() public {}
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool pool;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", "18");
+        token1 = new ERC20Mintable("USDC", "USDC", "18");
+    }
+
+    function testMintSuccess() public {
+        TestCaseParams memory params = TestCaseparams({
+            wethBalance: 1 ether,
+            usdcBalance: 5000 ether,
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240,
+            shouldTransferinCallback: true,
+            mintLiquidity: true
+        });
     }
 }
