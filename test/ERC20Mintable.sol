514eafffa2355ec73e08a255b084e22f472e92e6
Anton MetaZK <shonash88@gmail.com>
2023-07-16 01:22:44 +0300

    fix typo decimals on erc20mintable test

--- test/ERC20Mintable.sol
+++ test/ERC20Mintable.sol
@@ -7,8 +7,8 @@ contract ERC20Mintable is ERC20 {
     constructor(
         string memory _name,
         string memory _symbol,
-        uint8 _decimal
-    ) ERC20(_name, _symbol, _decimal) {}
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
