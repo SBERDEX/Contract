36076aa844e1d36fb0c0cc81d0ae4f5d8d35b272
Anton MetaZK <shonash88@gmail.com>
2023-07-20 04:32:24 +0300

    add flipped flag in Tick.sol lib)

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -12,10 +12,12 @@ library Tick {
         mapping(int24 => Tick.Info) storage self,
         int24 tick,
         uint128 liqudityDelta
-    ) internal {
+    ) internal returns (bool flipped) {
         Tick.Info storage tickInfo = self[tick];
         uint128 liquidityBefore = tickInfo.liqudity;
         uint128 liquidityAfter = liquidityBefore + liqudityDelta;
+
+        flipped = (liquidityAfter == 0) != (liquidityBefore == 0);
 
         if (liquidityBefore == 0) {
             tickInfo.initialized = true;
